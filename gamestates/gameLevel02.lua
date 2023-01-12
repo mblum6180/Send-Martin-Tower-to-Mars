2cd0eacbf5dc7d5cd309962d9cb313a0c9f1724c
MBlum6180 <Matthew@lightly-salted.com>
2023-01-12 14:35:30 -0500

    1435

--- gamestates/gameLevel02.lua
+++ gamestates/gameLevel02.lua
@@ -4,18 +4,18 @@ Gamestate = require 'libs.hump.gamestate'
 
 local gameLevel02 = {}
 function gameLevel02:init()
-    background = love.graphics.newImage("assets/marsmountain8bit.png") -- Background image
+    background = love.graphics.newImage("assets/spaceBG.png") -- Background image
 
     space:setCallbacks(beginContact, endContact, preSolve, postSolve)
 
     love.graphics.setBackgroundColor(0.92, 0.70, 0.60)
 
 
-    objects.ground.body = love.physics.newBody(space, system.winWidth/2, love.graphics.getPixelHeight() - 75)
+    objects.ground.body = love.physics.newBody(space, system.winWidth/2, system.winHeight)
     objects.ground.shape = love.physics.newRectangleShape(system.winWidth, 150)
     objects.ground.fixture = love.physics.newFixture(objects.ground.body, objects.ground.shape)
 
-    objects.tower.body = love.physics.newBody(space, love.math.random(0, system.winWidth), 0, "dynamic")
+    objects.tower.body = love.physics.newBody(space, love.math.random(0, system.winWidth), system.winHeight * 0.9, "dynamic")
     objects.tower.shape = love.physics.newRectangleShape(objects.tower.width, objects.tower.height + 75)
     objects.tower.fixture = love.physics.newFixture(objects.tower.body, objects.tower.shape, 1)
     objects.tower.fixture:setRestitution(0.3) 
@@ -26,25 +26,40 @@ end
 function gameLevel02:update(dt)
     space:update(dt) 
 
+    if objects.tower.body:getY() < 0 then
+        --objects.tower.fixture:destroy()
+        system.level02over = true
+    end
+
+
+    if system.level02over then
+        Gamestate.switch(gameLevel03)
+    end
+
     if love.keyboard.isDown("right") then
       objects.tower.body:applyForce(400, 0)
     elseif love.keyboard.isDown("left") then
       objects.tower.body:applyForce(-400, 0)
     end
     if love.keyboard.isDown("up") then
-        objects.tower.body:applyForce(0, -3000)
+        objects.fire.body = love.physics.newBody(space, objects.tower.body:getX() + 15, objects.tower.body:getY() - 50, "dynamic")
+        objects.fire.shape = love.physics.newRectangleShape(objects.fire.width, objects.fire.height)
+        objects.fire.fixture = love.physics.newFixture(objects.fire.body, objects.fire.shape, 1)
+        objects.fire.body:applyForce(0, -3000)
     end
 
 end
   
 function gameLevel02:draw()
     love.graphics.draw(background, 0, 0)
-    love.graphics.setColor(0.53, 0.39, 0.32)
-    love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints()))
+    --love.graphics.setColor(0.53, 0.39, 0.32)
+    --love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints()))
   
     love.graphics.setColor(1.0, 1.0, 1.0)
     love.graphics.draw(objects.tower.image, objects.tower.body:getX(), objects.tower.body:getY() )
-
+    if objects.fire.body then
+        love.graphics.draw(objects.fire.image, objects.fire.body:getX(), objects.fire.body:getY(), 0, 3, 3)
+    end
 
 end
 
--- objects.lua
+++ objects.lua
@@ -11,3 +11,8 @@ objects.tower.image = love.graphics.newImage("assets/martin8bit.png")
 objects.tower.width = objects.tower.image:getWidth()
 objects.tower.height = objects.tower.image:getHeight()
 
+
+objects.fire = {}
+objects.fire.image = love.graphics.newImage("assets/Fireball1.png")
+objects.fire.width = objects.fire.image:getWidth()
+objects.fire.height = objects.fire.image:getHeight()
